b9f01b2232c4a7fbbe558ffbf0c8acaf37aea19c
Diana001a <82045854+Diana001a@users.noreply.github.com>
2021-11-14 17:53:51 +0300

    e
    e

---
+++ Server/Server.DataAcess.Core/Models/UserRoleRto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Server.DataAcess.Core.Models
+{
+   [Table("User")]
+    class UserRoleRto
+    {
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Server/Server.DataAcess.Core/Models/UserRto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.DataAcess.Core.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+        [Key] public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Email { get; set; }
+
+        [Required] public string PhoneNumberPrefix { get; set; }
+
+        [Required] public string PhoneNumber { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTimeOffset Birthday { get; set; }
+
+        public string AvatarUrl { get; set; }
+
+        [Required] public UserRoleRto Role { get; set; }
+    }
+}
